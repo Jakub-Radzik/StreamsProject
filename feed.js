d7893e092e100ad5ca55f63acfa473e641851dde
Jakub Radzik <jradzik4@gmail.com>
2024-12-02 16:35:14 +0100

    more data

--- feed.js
+++ feed.js
@@ -1,7 +1,14 @@
 const pcap = require('pcap');
 const { Kafka } = require('kafkajs');
 
-const pcapFilePath = './big_1.pcap';
+const prefixFiles = './data';
+const fileNames = [
+  'DNS_amplify.pcap',
+  'ICMP_Flood.pcap',
+  'SYN_Flood.pcap',
+  'UDP_Flood.pcap',
+  // 'port_scan_from_192.168.1.69_to_192.168.1.4.pcap',
+];
 
 const kafka = new Kafka({
   clientId: 'my-kafka-producer',
@@ -19,59 +26,80 @@ async function sendBatch(batch) {
       topic: 'pcap',
       messages: batch,
     });
-    console.log(`Sent ${batch.length} packets to Kafka.`);
+    // console.log(`Sent ${batch.length} packets to Kafka.`);
   } catch (error) {
     console.error('Error sending batch to Kafka:', error);
   }
+}
+
+async function processPcapFile(filePath) {
+  return new Promise((resolve, reject) => {
+    try {
+      const pcapSession = pcap.createOfflineSession(filePath, 'ip');
+      console.log(`Starting to read PCAP file: ${filePath}`);
+
+      pcapSession.on('packet', async (rawPacket) => {
+        try {
+          const packet = pcap.decode.packet(rawPacket);
+          packetBatch.push({
+            value: JSON.stringify(packet),
+          });
+          packetCount++;
+
+          // Send the batch if it reaches the defined size
+          if (packetBatch.length >= BATCH_SIZE) {
+            const batchToSend = packetBatch;
+            packetBatch = []; // Reset batch
+            await sendBatch(batchToSend);
+          }
+        } catch (error) {
+          console.error('Error processing packet:', error);
+        }
+      });
+
+      pcapSession.on('complete', async () => {
+        console.log(`PCAP file read completed: ${filePath}`);
+
+        // Send any remaining packets
+        if (packetBatch.length > 0) {
+          // console.log(`Sending remaining ${packetBatch.length} packets...`);
+          await sendBatch(packetBatch);
+        }
+
+        console.log(
+          `Finished reading PCAP file: ${filePath}. ${packetCount} packets processed.`,
+        );
+        resolve();
+      });
+
+      pcapSession.on('error', (err) => {
+        console.error(`Error reading PCAP file (${filePath}): ${err.message}`);
+        reject(err);
+      });
+    } catch (err) {
+      console.error(`Failed to process PCAP file (${filePath}):`, err);
+      reject(err);
+    }
+  });
 }
 
 async function run() {
   try {
     await producer.connect();
 
-    const pcapSession = pcap.createOfflineSession(pcapFilePath, 'ip');
+    for (const filePath of fileNames) {
+      packetBatch = []; // Reset batch for each file
+      packetCount = 0; // Reset packet count for each file
+      const file = `${prefixFiles}/${filePath}`;
+      console.log(`Processing file: ${file}`);
 
-    console.log(`Starting to read PCAP file: ${pcapFilePath}`);
+      await processPcapFile(file);
+    }
 
-    pcapSession.on('packet', async (rawPacket) => {
-      try {
-        const packet = pcap.decode.packet(rawPacket);
-        packetBatch.push({
-          value: JSON.stringify(packet),
-        });
-        packetCount++;
-
-        // Send the batch if it reaches the defined size
-        if (packetBatch.length >= BATCH_SIZE) {
-          const batchToSend = packetBatch;
-          packetBatch = []; // Reset batch
-          await sendBatch(batchToSend);
-        }
-      } catch (error) {
-        console.error('Error processing packet:', error);
-      }
-    });
-
-    pcapSession.on('complete', async () => {
-      console.log('PCAP file read completed.');
-
-      // Send any remaining packets
-      if (packetBatch.length > 0) {
-        console.log(`Sending remaining ${packetBatch.length} packets...`);
-        await sendBatch(packetBatch);
-      }
-
-      console.log(
-        `Finished reading PCAP file: ${packetCount} packets processed.`,
-      );
-      await producer.disconnect();
-    });
-
-    pcapSession.on('error', (err) => {
-      console.error(`Error reading PCAP file: ${err.message}`);
-    });
+    console.log('All files processed successfully.');
+    await producer.disconnect();
   } catch (err) {
-    console.error('Failed to process PCAP file:', err);
+    console.error('Failed to process PCAP files:', err);
   }
 }
 
